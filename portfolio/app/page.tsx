ff2e3505e82a54ec65639a710e9959cd673595a6
aces96 <esraidi12@gmail.com>
2023-06-10 19:01:08 +0100

    i finished the introduction section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex flex-row justify-between h-full bg-zinc-800	">
-      <div className='w-80 h-screen flex items-center justify-center'>
+      <div className='w-80 h-screen flex items-center justify-center ml-5'>
         <div className='w-80 h-3/5 z-10 border-2 border-zinc-500 hover:border-green-500 rounded-3xl  bg-transparent flex flex-col items-center'>
             <Image className='rounded-3xl mt-7' alt='profilImage' src="/profilImg.png" width={170} height={120}/>
             <div className='w-full h-14 bg-transparent flex flex-col items-center justify-center'>
@@ -26,8 +26,33 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='w-9/12 h-full	bg-white'>
+      <div className='w-8/12 h-full mt-20'>
+        <div className='h-12 w-full  flex flex-row justify-start'>
+            <div className='w-36 h-full border-2 border-zinc-500 rounded-2xl flex flex-row items-center justify-center'>
+                <Image className='mr-2' alt='homeIcon' src="/home.svg" width={20} height={20}/>
+                <h5>
+                  Introduce
+                </h5>
+            </div>
+        </div>
 
+        <div className='h-80 w-full flex flex-col justify-center'>
+          <div className='flex flex-row mb-5'>
+              <h1 className='text-7xl '>
+                  Hi i'm
+              </h1>
+              <h1 className='text-7xl ml-6 text-green-500'>
+                  Achraf Esraidi
+              </h1>
+          </div>
+          <h1 className='text-7xl mb-5'>
+              Fullstack Developer
+          </h1>
+
+          <h5 className='text-zinc-500 text-lg'>
+              i build modern websites, mobile applications using latest web technologies.
+          </h5>
+        </div>
       </div>
 
     </main>
