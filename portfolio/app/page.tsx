d4bebdc07deaf502c653af032f4e04e8a3bd276b
aces96 <esraidi12@gmail.com>
2023-06-11 17:19:24 +0100

    i added services sections

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -65,15 +65,16 @@ export default function Home() {
           </h5>
         </m.div>
 
-        <div className='w-8/12 h-full mt-20'>
-          <div className='h-12 w-full  flex flex-row justify-start'>
+        <div className='h-12 w-full  flex flex-row justify-start'>
             <div className='w-36 h-full border-2 border-zinc-500 rounded-2xl flex flex-row items-center justify-center'>
                 <Image className='mr-2' alt='homeIcon' src="/resume.svg" width={20} height={20}/>
                 <h5>
                   Resume
                 </h5>
             </div>
-          </div>
+        </div>
+
+        <div className='w-full h-full mt-20 mb-16'>
 
           <VerticalTimeline s>
               <VerticalTimelineElement contentStyle={{ background: 'rgba(34, 192, 94,0.4)', color: '#fff' }}
@@ -107,8 +108,113 @@ export default function Home() {
                   June 2022 - Decembre 2022
                   </p>
               </VerticalTimelineElement>
+
+              <VerticalTimelineElement  contentStyle={{ background: 'rgba(34, 192, 94,0.4)', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgba(34, 192, 94,0.4)' }}
+                iconStyle={{ background: 'white', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', width: 50, height: 50}}
+                icon={<Image alt='workIcon' src={'/work.svg'} width={40} height={40}/>}
+                className="vertical-timeline-element--work">
+                <h3 className="vertical-timeline-element-title">Frontend Developer</h3>
+                <h4 className="vertical-timeline-element-subtitle">Internship, "IBTIKARCOM"</h4>
+                <p>
+                  i had an internship at the end of my first year at youcode school in a startup called "IBTIKARCOM" where i worked on the frontend side of a e-commerce platform using React js and Laravel it was my first professional experience where you get to work in big team you share idea and learn new skills from your teammates.
+                </p>
+                  <p className='text-green-500 '>
+                  June 2021 - Septembre 2021
+                  </p>
+              </VerticalTimelineElement>
+
+              <VerticalTimelineElement  contentStyle={{ background: 'rgba(34, 192, 94,0.4)', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgba(34, 192, 94,0.4)' }}
+                iconStyle={{ background: 'white', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', width: 50, height: 50}}
+                icon={<Image alt='educationIcon' src={'/education.svg'} width={40} height={40}/>}
+                className="vertical-timeline-element--work">
+                <h3 className="vertical-timeline-element-title">Web Development</h3>
+                <h4 className="vertical-timeline-element-subtitle">"Youcode School"</h4>
+                <p>
+                    i studied web development at youcode school for 2 years training program, where the first year we learn the basic of programming and the second year is where you choose the stack you want specialize in it i choosed "MERN stack".
+                </p>
+                  <p className='text-green-500 '>
+                  Septembre 2020 - june 2022
+                  </p>
+              </VerticalTimelineElement>
+
+              <VerticalTimelineElement  contentStyle={{ background: 'rgba(34, 192, 94,0.4)', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgba(34, 192, 94,0.4)' }}
+                iconStyle={{ background: 'white', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', width: 50, height: 50}}
+                icon={<Image alt='educationIcon' src={'/education.svg'} width={40} height={40}/>}
+                className="vertical-timeline-element--work">
+                <h3 className="vertical-timeline-element-title">baccalaureate</h3>
+                <h4 className="vertical-timeline-element-subtitle">"Jaber ibn Hayan High School"</h4>
+                <p>
+                    i had my electrical science baccalaureate from jaber ibn hayan high school.
+                </p>
+                  <p className='text-green-500 '>
+                  june 2017 
+                  </p>
+              </VerticalTimelineElement>
           </VerticalTimeline>
         </div>
+
+        <div className='h-12 w-full  flex flex-row justify-start'>
+            <div className='w-36 h-full border-2 border-zinc-500 rounded-2xl flex flex-row items-center justify-center'>
+                <Image className='mr-2' alt='homeIcon' src="/service.png" width={20} height={20}/>
+                <h5>
+                  Services
+                </h5>
+            </div>
+        </div>
+
+        <div className='w-full h-full mt-16 mb-16 flex flex-col items-start'>
+            <div className=' flex flex-row'>
+              <h2 className='text-7xl'>
+              My
+              </h2>
+              <h2 className='text-7xl text-green-500 ml-3'>
+                    Services
+              </h2>
+              <h2 className='text-7xl '>
+                    .
+              </h2>
+            </div>
+
+            <m.div
+            initial={{y: "40%"}}
+            whileInView={{y: '0%'}}
+            transition={{duration: 0.6}}
+            className='w-10/12 h-40 border-2 border-zinc-500 hover:border-green-500 mt-10 rounded-2xl flex flex-col items-center justify-center'>
+                <div className='w-10/12 h-10 flex flex-row items-center justify-between'>
+                    <h5 className='text-2xl'>
+                    Web development
+                    </h5>
+                    <Image alt='codeIcon' src='/codeIcon.png' width={30} height={30}/>
+                </div>
+                <h5 className='text-base text-zinc-400 text-left mt-4'>
+                    i build modern web apps using the latest web technologies.
+                    like "Reactjs, Nodejs, Nextjs...""
+                </h5>
+            </m.div>
+
+            <m.div
+            initial={{y: "40%"}}
+            whileInView={{y: '0%'}}
+            transition={{duration: 0.6}}
+            className='w-10/12 h-40 border-2 border-zinc-500 hover:border-green-500 mt-10 rounded-2xl flex flex-col items-center justify-center'>
+                <div className='w-10/12 h-10 flex flex-row items-center justify-between'>
+                    <h5 className='text-2xl'>
+                    Mobile development
+                    </h5>
+                    <Image alt='codeIcon' src='/codeIcon.png' width={30} height={30}/>
+                </div>
+                <h5 className='text-base text-zinc-400 text-left mt-4'>
+                    i build modern mobile applications React Native.
+                </h5>
+            </m.div>
+
+        </div>
+
+
+
       </div>
     </main>
   )
